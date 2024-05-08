3bce8e2c054b3f2de4143419761891481bbbc4e8
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2024-05-08 06:35:56 +0100

    implemented sigin and signup endpoint

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
   {
@@ -28,6 +29,24 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
+//Login
+userSchema.methods.matchPassword = async function(enterPassword) {
+  return await bcrypt.compare(enterPassword, this.password)
+}
 
+//Register
+userSchema.pre("save", async function(next){
+  if(!this.isModified("password")){
+      next();
+  }
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+const User = mongoose.model('User', userSchema);
 export default User;
+
+
+
+
+
