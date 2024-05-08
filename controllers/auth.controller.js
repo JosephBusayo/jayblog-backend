e0c4179e323e22a83289640ed953b763f195ef9d
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2024-05-08 06:22:07 +0100

    set up user authentication

---
+++ controllers/auth.controller.js
@@ -0,0 +1,99 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/user.model.js";
+import generateTOken from './../utils/generateToken.js';
+import bcryptjs from "bcryptjs"; // For password hashing
+import { errorHandler } from "../utils/error.js";
+
+
+
+//LOGIN
+export const signin = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            username: user.username,
+            profilePicture: user.profilePicture,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            createdAt: user.createdAt,
+            token: generateTOken(user._id),
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid Email or Pasword')
+    }
+})
+
+//REGISTER
+export const signup = asyncHandler(async (req, res) => {
+    const { username, email, password, profilePicture } = req.body
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error("User already exists")
+    }
+    const user = await User.create({
+        username, email, password, profilePicture
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            profilePicture: user.profilePicture,
+            isAdmin: user.isAdmin,
+            token: generateTOken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error("Invalid User Data")
+    }
+})
+
+
+/* export const google = async (req, res, next) => {
+    const { name, email, googlePhotoUrl } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (user) {
+            const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET);
+            const { password, ...rest } = user._doc;
+            res
+                .status(200)
+                .cookie("access_token", token, {
+                    httpOnly: true,
+                })
+                .json(rest);
+        } else {
+            const generatedPassword =
+                Math.random().toString(36).slice(-8) +
+                Math.random().toString(36).slice(-8);
+            const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+            const newUser = new User({
+                username:
+                    name.toLowerCase().split(" ").join("") +
+                    Math.random().toString(9).slice(-4),
+                email,
+                password: hashedPassword,
+                profilePicture: googlePhotoUrl,
+            });
+            await newUser.save();
+            const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET);
+            const { password, ...rest } = newUser._doc;
+            res
+                .status(200)
+                .cookie("access_token", token, {
+                    httpOnly: true,
+                })
+                .json(rest);
+        }
+    } catch (error) {
+        next(error);
+    }
+};
+ */
--- server.js
+++ server.js
@@ -3,6 +3,14 @@ import express from 'express';
 import cors from 'cors';
 import connectDatabase from './config/MongoDb.js';
 
+import authRoutes from './routes/auth.route.js'
+
+/* import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js"; */
+import cookieParser from "cookie-parser";
+import path from "path";
 
 
 dotenv.config();
@@ -16,7 +24,10 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use("/api/auth", authRoutes);
+app.use('/test', (req, res) => {
+    res.send("Seen")
+})
 // Start the server
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
